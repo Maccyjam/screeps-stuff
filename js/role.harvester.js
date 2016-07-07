d527bc6f5251bd324aa0063bc25b112c8a77db87
James MacBeth <maccyjam@gmail.com>
2016-07-07 03:21:40 +0100

    Harvesters ensure spawn is full before filling other structures

--- js/role.harvester.js
+++ js/role.harvester.js
@@ -22,23 +22,46 @@ var roleHarvester = {
         }
         else {
             // Fill all empty structures!
-            var emptyStructures = creep.room.find(FIND_MY_STRUCTURES, {
+            var emptyStructures = creep.room.find(FIND_STRUCTURES, {
                 filter: function (object) {
-                    return object.energy < object.energyCapacity;;
-                }
-            });
-        
-            if (emptyStructures[0] == STRUCTURE_CONTAINER) {
-                if (creep.carry.energy > creep.carryCapacity) { // Only repair with half of its energy.
-                    if (creep.repair(emptyStructures[0]) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(emptyStructures[0]);
+                    if (object.structureType != STRUCTURE_CONTAINER){
+                        return object.energy < object.energyCapacity;
+                    } else {
+                        return object.store.energy < object.storeCapacity;
                     }
                 }
+            });
+            
+            creep.say(emptyStructures.length); // For Debuging...
+            
+            // TODO: Set up preference. Spawn first, then extensions, then anything else.
+            if (Game.spawns.Spawn1.energy != Game.spawns.Spawn1.energyCapacity) { // This makes sure spawn is full before anything else is filled.
+                if (creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(Game.spawns.Spawn1);
+                }
+                
+            } else { // If spawn is full...
+                // Handle container filling...
+                if (emptyStructures[0].structureType == STRUCTURE_CONTAINER) {
+                    if (creep.carry.energy > (creep.carryCapacity / 2)) { // Only fill with half of its energy.
+                        if (creep.transfer(emptyStructures[0], RESOURCE_ENERGY, (creep.carry.energy / 2)) == ERR_NOT_IN_RANGE) {
+                            creep.moveTo(emptyStructures[0]);
+                        } 
+                    } else {
+                        // Repair with rest.
+                        if (creep.repair(emptyStructures[0]) == ERR_NOT_IN_RANGE) {
+                            creep.moveTo(emptyStructures[0]);
+                        }
+                    }
+                }
+                
+                // Fill everything else.
+                if (creep.transfer(emptyStructures[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(emptyStructures[0]);
+                }
             }
-        
-            if (creep.transfer(emptyStructures[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(emptyStructures[0]);
-            }
+            
+            
             
             // if (Game.spawns.Spawn1.energy != Game.spawns.Spawn1.energyCapacity) {
             //     if(creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
